d4bdbcc0279632b1865a0eb214a98a84dd830c7f
Aditya Pathak <pathakaditya6266@gmail.com>
2026-09-20 18:07:43 +0530

    add some error files

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+    constructor(statusCode, data, message = "Successs"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.statusCode = statusCode < 400
+    }
+}
